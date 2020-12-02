6280044d7bfbaf193ed8d0d75617f3897e72c282
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-12-02 11:31:29 +0100

    mvn license & format

--- src/java/fr/paris/lutece/plugins/forms/modules/userassignment/business/form/querypart/impl/FormColumnFormResponseAssigneeQueryPart.java
+++ src/java/fr/paris/lutece/plugins/forms/modules/userassignment/business/form/querypart/impl/FormColumnFormResponseAssigneeQueryPart.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2020, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.forms.modules.userassignment.business.form.querypart.impl;
 
 import java.util.HashMap;
@@ -17,16 +50,17 @@ import fr.paris.lutece.portal.service.spring.SpringContextService;
 public class FormColumnFormResponseAssigneeQueryPart extends AbstractFormColumnQueryPart
 {
     public static final String KEY_USER_LIST = "user_list";
-    
+
     private IResourceUserDAO resourceUserDAO = SpringContextService.getBean( IResourceUserDAO.BEAN_NAME );
-    
+
     @Override
     protected Map<String, Object> getMapFormColumnValues( FormResponseSearchItem formResponseSearchItem )
     {
         Map<String, Object> mapFormColumnValues = new HashMap<>( );
-        
-        List<AdminUser> userList = resourceUserDAO.selectUserListByResource( formResponseSearchItem.getIdFormResponse( ), FormResponse.RESOURCE_TYPE, FormsPlugin.getPlugin( ) );
-        
+
+        List<AdminUser> userList = resourceUserDAO.selectUserListByResource( formResponseSearchItem.getIdFormResponse( ), FormResponse.RESOURCE_TYPE,
+                FormsPlugin.getPlugin( ) );
+
         if ( CollectionUtils.isNotEmpty( userList ) )
         {
             mapFormColumnValues.put( KEY_USER_LIST, userList );
